32164491f24ce2b3134bf92e6704a0b3232d5faf
jong-jang <jjh0299@gmail.com>
2023-06-19 10:44:02 +0900

    세션, 플래시, 정적 파일 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express'); // express
 const path = require('path'); // 경로 설정
 const mongoose = require('mongoose'); // db 연결
 const ejsMate = require('ejs-mate'); // 템플릿
+const session = require('express.session'); // 세션
+const flash = require('connect-flash') // 플래시, 문구 띄우기
 const ExpressError = require('./utils/ExpressError') // express 에러 처리
 const methodOverride = require('method-override'); // update, delete 메소드
 const morgan = require('morgan'); // 로그 출력
@@ -33,7 +35,28 @@ morgan('tiny');
 app.use(express.urlencoded({extended:true}));
 // methodoverride
 app.use(methodOverride("_method"));
-
+// public 폴더 정적 asset 설정
+app.use(express.static(path.join(__dirname, 'public'))); 
+// 세션 설정값
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!', // 비밀키
+    resave: false, // 세션 중단 경고 메시지 처리
+    saveUninitialized: true, // 세션 중단 경고 메시지 처리
+    cookie: {
+        httpOnly: true, // 서버로 전송되는 쿠키를 JavaScript로 접근하는 것을 막는 옵션
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // 쿠키가 만료되는 날짜 (설정 : 일주일)
+        maxAge : 1000 * 60 * 60 * 24 * 7 // 현재 시간부터 MaxAge 시간이 경과하면 쿠키가 만료
+    }
+}
+// 세션
+app.use(session(sessionConfig));
+// 플래시
+app.use(flash());
+app.use((req, res ,next) => {
+    req.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 // 라우트
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
